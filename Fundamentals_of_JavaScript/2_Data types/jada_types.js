7f5f6139d565c6376cce0fe7825c28d707be28eb
Rajinikanth <mail2rajinikanthb@gmail.com>
2026-01-09 16:37:14 +0530

    added data_types.js file

---
+++ Fundamentals_of_JavaScript/2_Data types/jada_types.js
@@ -0,0 +1,10 @@
+// In JS, variables can be created using the following keywords:
+// 1. var
+// 2. let
+// 3. const
+
+var name = "Ajay"
+let age = 30
+const gender = "Male"
+
+console.log(`Hello, ${name}! you are ${age} years old.`)
